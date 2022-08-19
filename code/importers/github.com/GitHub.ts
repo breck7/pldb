f944f1df12218d0c198ec8333c9ad391edc6288c
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2022-08-19 09:36:21 -1000

    GH Filenames

--- code/importers/github.com/GitHub.ts
+++ code/importers/github.com/GitHub.ts
@@ -255,12 +255,14 @@ class GitHubImporter {
 	async writeLanguageDataCommand() {
 		this.matched.forEach(match => {
 			const { file, lang } = match
-			const { type, title, extensions, group } = lang
+			const { type, title, extensions, group, filenames } = lang
 			const ghNode = file.touchNode("githubLanguage")
 			if (!ghNode.getContent()) ghNode.setContent(title)
 			ghNode.set("type", type)
 			if (extensions)
 				ghNode.set("fileExtensions", extensions.join(" ").replace(/\./g, ""))
+
+			if (filenames) ghNode.set("filenames", filenames.join(" "))
 
 			if (group) {
 				ghNode.set("group", group)
