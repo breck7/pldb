9bb79cb7fc09634f6ccaf78ca5a3f00fddc898df
Breck Yunits <breck7@gmail.com>
2022-09-21 04:56:48 -1000

    checkpoint

--- code/Interfaces.ts
+++ code/Interfaces.ts
@@ -1,46 +1,51 @@
 interface FeatureSummary {
-    feature: string
-    featureLink: string
-    aka: string
-    path: string
-    token?: string
-    yes: number
-    no: number
-    percentage: string
-    pseudoExample: string
-  }
-  
-  interface FeaturePrediction {
-    value: boolean
-    token?: string
-    example?: string
-  }
-  
-  interface Example {
-    code: string
-    source: string
-    link: string
-  }
+  feature: string
+  featureLink: string
+  aka: string
+  path: string
+  token?: string
+  yes: number
+  no: number
+  percentage: string
+  pseudoExample: string
+}
 
-  interface FileInterface {
+interface FeaturePrediction {
+  value: boolean
+  token?: string
+  example?: string
+}
 
-  }
+interface Example {
+  code: string
+  source: string
+  link: string
+}
 
-  interface FolderInterface {
-    prettifyContent: Function
-    predictNumberOfJobs: Function
-    predictPercentile: Function
-    predictNumberOfUsers: Function
-    getFileAtLanguageRank: Function
-    getFileAtRank: Function
-    getFeatureAtRank: Function
-    getLanguageRankExplanation: Function
-    getFeatureRank: Function
-    columnDocumentation: any[]
-    topLanguages: any[]
-    getRank: Function
-    getLanguageRank: Function
-    getFile: Function
-  }
+interface FileInterface {}
 
-export { FeatureSummary, FeaturePrediction, Example, FolderInterface, FileInterface }
+interface FolderInterface {
+  prettifyContent: Function
+  predictNumberOfJobs: Function
+  predictPercentile: Function
+  predictNumberOfUsers: Function
+  getFileAtLanguageRank: Function
+  getFileAtRank: Function
+  getFeatureAtRank: Function
+  getLanguageRankExplanation: Function
+  getFeatureRank: Function
+  columnDocumentation: any[]
+  topLanguages: any[]
+  getRank: Function
+  getLanguageRank: Function
+  getFile: Function
+  featuresMap: any
+}
+
+export {
+  FeatureSummary,
+  FeaturePrediction,
+  Example,
+  FolderInterface,
+  FileInterface
+}
