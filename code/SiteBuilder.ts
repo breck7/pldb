27d068058cce4fc0d91ffed8b7ccb36f2131145f
Breck Yunits <breck7@gmail.com>
2022-09-25 19:00:19 -1000

    checkpoint

--- code/SiteBuilder.ts
+++ code/SiteBuilder.ts
@@ -560,7 +560,10 @@ class SiteBuilder {
         )
         .join(" · "),
       NEW_POSTS: postsScroll.files
-        .filter(file => !file.groups.includes("publicDomainCompany")) // keep front page posts focused on pls
+        .filter(
+          file =>
+            !file.scrollScriptProgram.toString().includes("publicDomainCompany")
+        ) // keep front page posts focused on pls
         .slice(0, 5)
         .map(file => `<a href="./posts/${file.permalink}">${file.title}</a>`)
         .join("<br>")
