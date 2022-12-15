b9832f8399ca98e01677f4af19cac5571d82fdc7
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2022-12-15 06:53:28 -0800

    Stats cron update

---
+++ site/updateStats.sh
@@ -0,0 +1,3 @@
+# This file is in the site/ folder because we want it on the Nginx server
+goaccess /var/log/nginx/access.log -o /var/www/html/nginxDaily.html --log-format=COMBINED --anonymize-ip
+zcat -f /var/log/nginx/access.log* | goaccess -o /var/www/html/nginxDailyAll.html --log-format=COMBINED --anonymize-ip
