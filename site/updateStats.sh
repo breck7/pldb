8c746d45b64d9cf9d0965e0aa0c7de41ef5d8ebb
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2023-02-03 08:38:40 -0800

    Update traffic links

--- site/updateStats.sh
+++
@@ -1,3 +0,0 @@
-# This file is in the site/ folder because we want it on the Nginx server
-goaccess /var/log/nginx/access.log -o /var/www/html/nginxDaily.html --log-format=COMBINED --anonymize-ip
-zcat -f /var/log/nginx/access.log* | goaccess - -o /var/www/html/nginxDailyAll.html --log-format=COMBINED --anonymize-ip
