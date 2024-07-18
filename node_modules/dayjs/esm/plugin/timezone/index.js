7c0e98db00e68df82ac977f34e196256aeaf0772
breck7 <breck7@gmail.com>
2024-07-18 17:27:43 +0000

    Deploying to wws from @ breck7/pldb@3a9f70c03b1856b4ac50bd588b5271d73c45f84d 🚀

--- node_modules/dayjs/esm/plugin/timezone/index.js
+++ node_modules/dayjs/esm/plugin/timezone/index.js
@@ -120,13 +120,22 @@ export default (function (o, c, d) {
       timeZone: timezone
     });
     var diff = Math.round((date - new Date(target)) / 1000 / 60);
-    var ins = d(target, {
-      locale: this.$L
-    }).$set(MS, this.$ms).utcOffset(-Math.round(date.getTimezoneOffset() / 15) * 15 - diff, true);
+    var offset = -Math.round(date.getTimezoneOffset() / 15) * 15 - diff;
+    var isUTC = !Number(offset);
+    var ins;
 
-    if (keepLocalTime) {
-      var newOffset = ins.utcOffset();
-      ins = ins.add(oldOffset - newOffset, MIN);
+    if (isUTC) {
+      // if utcOffset is 0, turn it to UTC mode
+      ins = this.utcOffset(0, keepLocalTime);
+    } else {
+      ins = d(target, {
+        locale: this.$L
+      }).$set(MS, this.$ms).utcOffset(offset, true);
+
+      if (keepLocalTime) {
+        var newOffset = ins.utcOffset();
+        ins = ins.add(oldOffset - newOffset, MIN);
+      }
     }
 
     ins.$x.$timezone = timezone;
